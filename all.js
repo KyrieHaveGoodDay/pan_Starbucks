e93269b70c2458043b08d28de1278d5ee390089d
kyriehavegoodday <healthymbear@gmail.com>
2022-01-25 10:00:55 +0800

    檔案更新

---
+++ all.js
@@ -0,0 +1,16 @@
+
+$('#slide').slick({
+    autoplay:true,
+});
+console.log('123');
+
+// // jquery
+// $('#box').click(function () { });
+
+// // js
+// let chick = document.getElementById('box');
+// chick.addEventListener('click', function () {
+
+// })
+// $('#slide').slick();
+
